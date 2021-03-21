1c51efe1b767774925079e01aee91c218d68a0a3
IIIndex <59024793+IIIndex@users.noreply.github.com>
2021-03-21 12:14:01 +0800

    Create enhance.h

---
+++ src/enhance/enhance.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<opencv2\core\core.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp> 
+#define _CRT_SECURE_NO_WARNINGS
