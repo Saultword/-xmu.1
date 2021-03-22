9b6f1d421ffe327c8bb1010f2fe740919483680a
zhengrenjie2714 <64193383+zhengrenjie2714@users.noreply.github.com>
2021-03-22 13:13:44 +0800

    Add files via upload

---
+++ src/module3/module3_main.cpp
@@ -0,0 +1,31 @@
+#include "module3.h"
+
+using namespace module3;
+
+int main()
+{
+	char window_name[15] = "Module3_2Dcode";
+
+	//创建显示窗口
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+
+	//创建一个图片流输入、输出接口
+	vector<Mat> src_images;
+
+	//测试接口
+	Mat src_image = imread("./1.jpg");
+
+	src_images.push_back(src_image);
+
+	src_images = get_QRcode(src_images);
+	
+	src_images[0] = enhancement(src_images[0]);
+
+	//显示结果
+	imshow(window_name, src_images[0]);
+	imwrite("./duibi1.jpg", src_images[0]);
+	// 等待用户反应
+	waitKey(0);
+
+	return 0;
+}
