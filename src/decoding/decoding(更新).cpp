c6d6c96e0b7195f9ef658a7b956a8166e1b6947d
Fjh <80078976+Mayes-byte@users.noreply.github.com>
2021-03-25 21:43:38 +0800

    Add files via upload

---
+++ src/decoding/decoding(更新).cpp
@@ -0,0 +1,23 @@
+#include "decoding.h"
+
+using namespace enAndde_code;
+
+int main() {
+//	ofstream infile("..\\in.txt", ios::out);								//这两行用于随机生成txt文件
+//	form(infile);															//用的时候将其他行注释掉（下四行）
+//	ifstream infile("..\\in.txt", ios::in);
+//	ofstream outfile("..\\out_01.txt", ios::out && ios::binary);
+//	encode(infile);															//将infile中的内容编码，生成二维码
+//	decode(outfile);														//将二维码解码，并写入到outfile中
+	decode();	
+	vector<string>::iterator ite = pool.begin();
+	for (; ite != pool.end(); ite++) {
+		cout << *ite << endl;
+	}
+	cout << endl;
+
+//	outfile.close();		
+//	infile.close();
+	return 0;
+}
+
