a6c5f3439b15ab332d91fe1457aa80d9382da6ff
Saultword <56984286+Saultword@users.noreply.github.com>
2021-04-02 09:49:25 +0800

    Update coding.cpp

--- src/coding/coding.cpp
+++ src/coding/coding.cpp
@@ -1,133 +1,72 @@
-#include "coding.h"
-using namespace std;
-using namespace cv;
-
-namespace encoding {
-
-    //单张图片存储二进制位最大值
-    const int DATA_LENGTH = 100;
-
-    const int MAX_SIZE = DATA_LENGTH * DATA_LENGTH - 8;
-
-    const int PIX_SIZE = 8;
-
-    //读文件+返回string
-    string getFile() {
-
-        //打开文件
-        string rt;
-        FILE* fp = fopen("data.in", "r");
-        if (fp == nullptr) {
-            cout << "Open file failed! " << endl;
-            return rt;
-        }
-
-        //读取文件
-        char ch;
-        while ((ch = fgetc(fp)) != EOF)rt += ch;
-
-        return rt;
-    }
-
-    //文件转化成二进制
-    string file2Binary(string s) {
-        string rt;
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
-            char ch = s[i];
-            for (int j = 0; j < 8; j++) {
-                if (ch % 2)rt += '1';
-                else rt += '0';
-                ch /= 2;
-            }
-        }
-        return rt;
-    }
-
-    //待写入文件分块
-    vector<string> split(string s) {
-        vector<string> rt;
-        int len = s.size();
-        int head = 0, end = min(head + MAX_SIZE, len);
-        while (head != len) {
-            string tmp;
-            for (int i = head; i < end; i++) {
-                tmp.push_back(s[i]);
-            }
-            rt.push_back(tmp);
-            head = end;
-            end = min(head + MAX_SIZE, len);
-        }
-        return rt;
-    }
-
-    //将string内存的二进制信息存入Mat
-    Mat string2Mat(string s, int num) {
-        Mat img(DATA_LENGTH, DATA_LENGTH, CV_8UC1);
-
-        //存入张数信息
-        for (int i = 7; i >= 0 ; i--) {
-            img.ptr<uchar>(0)[i] = (num % 2  ? 255 : 0);
-            num /= 2;
-        }
-
-        //存入数据
-        int cnt = 0, len = s.size();
-        for (int i = 0; i < DATA_LENGTH; i++) {
-            for (int j = 0; j < DATA_LENGTH; j++) {
-                if (i == 0 && j < 8)continue;
-                if (cnt >= len)img.ptr<uchar>(i)[j] = 0;
-                img.ptr<uchar>(i)[j] = (s[cnt++] == '1' ? 255 : 0);
-            }
-        }
-
-        //放大图片
-        Mat bigger_img(800, 800, CV_8UC1);
+namespace picvideo
 
 
-        for (int i = 0; i < 800; i++) {
-            for (int j = 0; j < 800; j++) {
-                int x = i / PIX_SIZE;
-                int y = j / PIX_SIZE;
-                bigger_img.ptr<uchar>(i)[j] = img.ptr<uchar>(x)[y];
-            }
-        }
+{
+	using namespace std;
+	using namespace cv;
+	void pic2video(vector<Mat>& src_ims)
+	{
+		char name2[20];
+		cout << "请输入编码后的视频文件名，例如“test.mp4”:" << endl;
+		cin >> name2;
+		int frame_rate;
+		cout << "请输入视频帧率:" << endl;
+		cin >> frame_rate;
+		VideoWriter video(name2, CAP_ANY, frame_rate, Size(1000, 1000));
+		for (size_t i = 0; i < src_ims.size(); i++)
+		{
+			if (i == src_ims.size())
+			{
+				for (int j = 0; j < frame_rate; j++)
+				{
+					Mat image = src_ims[i].clone();
+					video << image;
+				}
+			}
+			else
+			{
+				Mat image = src_ims[i].clone();
+				// 流操作符，把图片传入视频
+				video << image;
+			}
+		}
+	}
+	void video2pic(string path, vector<Mat>& src_ims)
+	{
+		VideoCapture capture(path);
+		Mat frame;
+		while (true) {
+			//一帧一帧读
+			capture >> frame;
+			if (frame.empty()) {
+				break;
+			}
+			else src_ims.push_back(frame.clone());
+		}
+	}
+
+	void videoshow(string path)
+	{
+		VideoCapture capture(path);
+		while (1)
+		{
+			//frame存储每一帧图像
+			Mat frame;
+			//读取当前帧
+			capture >> frame;
+			//播放完退出
+			if (frame.empty()) {
+				printf("播放完成\n");
+				break;
+			}
+			//显示当前视频
+			imshow("读取视频", frame);
+			//延时300ms
+			waitKey(300);
+		}
 
 
-        //添加轮廓
-        Mat rt(1000, 1000, CV_8UC1);
-
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < 1000; j++) {
-                rt.ptr<uchar>(i)[j] = 255;
-                if (i <= 916 && i >= 84 && j <= 916 && j >= 84) {
-                    rt.ptr<uchar>(i)[j] = 0;
-                }
-                /*现在黑框只有4pixel*/
-                if (i <= 912 && i >= 88 && j <= 912 && j >= 88) {
-                    rt.ptr<uchar>(i)[j] = 255;
-                }
-            }
-        }
-
-        //往轮廓内添加信息
-        for (int i = 100; i < 900; i++) {
-            for (int j = 100; j < 900; j++) {
-                rt.ptr<uchar>(i)[j] = bigger_img.ptr<uchar>(i - 100)[j - 100];
-            }
-        }
-
-        return rt;
-    }
-
-    //批量处理
-    vector<Mat> string2Mat_vector(vector<string> s) {
-        int len = s.size();
-        vector<Mat> rt;
-        for (int i = 0; i < len; i++) {
-            Mat tmp = string2Mat(s[i], i + 1);
-            rt.push_back(tmp);
-        }
-        return rt;
-    }
+	}
 }
+
+
